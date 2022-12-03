7a965182694b1a23c6c930c0b480150dfe4428c9
Derek Rymers <rymersd@gmail.com>
2022-12-03 09:42:55 -0500

    Done with db

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,9 +1,21 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users, user_data, insulin, insulin_data, insulin_data_join, meals, blood_sugar CASCADE;
-DROP SEQUENCE IF EXISTS seq_user_id;
+DROP TABLE IF EXISTS users, user_data, insulin, insulin_data, insulin_data_join, meals, meals_user_join, blood_sugar CASCADE;
+DROP SEQUENCE IF EXISTS seq_user_id, seq_insulin_id, meal_id CASCADE;
 
 CREATE SEQUENCE seq_user_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+
+CREATE SEQUENCE seq_insulin_id
+  INCREMENT BY 1
+  NO MAXVALUE
+  NO MINVALUE
+  CACHE 1;
+  
+CREATE SEQUENCE seq_meal_id
   INCREMENT BY 1
   NO MAXVALUE
   NO MINVALUE
@@ -27,35 +39,38 @@ CREATE TABLE user_data (
 	a1c decimal(4, 2),
 	fasting_glucose int,
 	diabetes_type int,
+	user_age int,
+	last_updated TIMESTAMP DEFAULT CURRENT_DATE,
+	age int,
+	weight int,
+	height int,
+	-- bmi (weight/height)**2 calc on server
 	
 	CONSTRAINT FK_user_data_user FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE insulin (
 	user_id int NOT NULL PRIMARY KEY,
-	insulin_type varchar(32) NOT NULL UNIQUE, --Fast acting, long acting, glucagon, etc.
 	base_level decimal(10,3),
 	avg_level decimal(10,3),
-	date_updated TIMESTAMP DEFAULT CURRENT_DATE,
+	time_last_dose TIMESTAMP,
 	
 	CONSTRAINT FK_insulin_user FOREIGN KEY (user_id) REFERENCES user_data(user_id)
 );
 
 CREATE TABLE insulin_data (
-	insulin_id serial PRIMARY KEY,
+	insulin_id int DEFAULT nextval('seq_insulin_id'::regclass) NOT NULL UNIQUE,
 	insulin_type varchar(32) NOT NULL UNIQUE,
 	insulin_strength varchar(32),
 	half_life int, --minutes
 	onset int, --display information for when to check blood sugar
 	peak int,
-	duration int,
-
-	CONSTRAINT FK_insulin_data_insulin FOREIGN KEY (insulin_type) REFERENCES insulin(insulin_type)
+	duration int
 );
 
 CREATE TABLE insulin_data_join (
-	user_id bigint,
-	insulin_id bigint,
+	user_id int,
+	insulin_id int,
 	
 	CONSTRAINT PK_insulin_data_join PRIMARY KEY (user_id, insulin_id),
 	CONSTRAINT FK_insulin_data_join_insulin FOREIGN KEY (user_id) REFERENCES insulin(user_id),
@@ -63,14 +78,20 @@ CREATE TABLE insulin_data_join (
 );
 
 CREATE TABLE meals (
-	meal_id serial PRIMARY KEY,
-	user_id int NOT NULL,
+	meal_id int DEFAULT nextval('seq_meal_id'::regclass) NOT NULL unique,
 	carbs int NOT NULL,
 	food varchar (32),
 	glycemic_index int, --FDA API
-	date_updated TIMESTAMP DEFAULT CURRENT_DATE,
+	meal_time TIMESTAMP DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE meals_user_join (
+	meal_id int NOT NULL,
+	user_id int NOT NULL,
 	
-	CONSTRAINT FK_meals_user FOREIGN KEY (user_id) REFERENCES user_data(user_id)
+	CONSTRAINT PK_meals_user_join PRIMARY KEY (meal_id, user_id),
+	CONSTRAINT FK_meals_user_join_user_data FOREIGN KEY (user_id) REFERENCES user_data(user_id),
+	CONSTRAINT FK_meals_user_join_meals FOREIGN KEY (meal_id) REFERENCES meals(meal_id)
 );
 
 CREATE TABLE blood_sugar (
@@ -78,7 +99,7 @@ CREATE TABLE blood_sugar (
 	target_low int NOT NULL,
 	target_high int NOT NULL,
 	input_level int NOT NULL,
-	date_updated TIMESTAMP DEFAULT CURRENT_DATE,
+	last_measurement TIMESTAMP DEFAULT CURRENT_DATE,
 	
 	CONSTRAINT FK_blood_sugar_users FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
