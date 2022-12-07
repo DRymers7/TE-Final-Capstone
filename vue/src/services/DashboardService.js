17f8b53302a4c39c8472a0b3506e5bbc4220ea21
Mitchell Brown <19mitch99@gmail.com>
2022-12-07 13:46:36 -0500

    fixed dash

--- vue/src/services/DashboardService.js
+++ vue/src/services/DashboardService.js
@@ -2,7 +2,8 @@ import axios from 'axios';
   
 
 export default {
-    postNewMeal(Meal) {
-        return axios.post('/', Meal) 
+    postNewReading() {
+        return axios.post('/') 
       },
+    
 }
