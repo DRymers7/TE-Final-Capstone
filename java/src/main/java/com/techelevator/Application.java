58f410cd11e3dc572e8a9b92773b41e279de72aa
Derek Rymers <rymersd@gmail.com>
2022-12-06 20:29:40 -0500

    Trying to resolve intelliJ problem

--- java/src/main/java/com/techelevator/Application.java
+++ java/src/main/java/com/techelevator/Application.java
@@ -7,12 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(Application.class, args);
-        } catch (Throwable e) {
-            e.printStackTrace();
-        }
-
+        SpringApplication.run(Application.class, args);
     }
 
 }
