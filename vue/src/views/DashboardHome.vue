e46802806300e42a0ae3afb4a2d1160f194a1ba4
Derek Rymers <rymersd@gmail.com>
2022-12-10 11:24:24 -0500

    Pushing dashboard template

---
+++ vue/src/views/DashboardHome.vue
@@ -0,0 +1,19 @@
+<template>
+  
+<div>
+    <DashboardHome />
+</div>
+</template>
+
+<script>
+import DashboardHome from "../components/DashboardHome.vue"
+export default {
+    components: {
+        DashboardHome
+    }
+}
+</script>
+
+<style>
+
+</style>
