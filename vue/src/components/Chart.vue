1c5b3eac433d925bb134d484e38b36fea14a13cb
Evan Hepp <evan.hepp@gmail.com>
2022-12-14 11:24:54 -0500

    pulling and pushing chart with input data

--- vue/src/components/Chart.vue
+++ vue/src/components/Chart.vue
@@ -57,9 +57,8 @@ export default {
           id: "vuechart",
         },
         xaxis: {
-          type: "datetime",
-          min: "",
-          max: "",
+          min: 1,
+          max: 200,
           tickAmount: 5,
         },
         yaxis: {
@@ -99,11 +98,16 @@ export default {
   },
 
   methods: {
-    updateChart() {
+    updateChart(response) {
+      response = this.series[0].data;
       const newData = this.series[0].data;
+      console.log(this.series[0].data);
+      console.log(this.series[0].data.length);
+      this.series[0].data = response;
+      for (var i = 0; i < newData.length; i++) {
+        this.series[0].data = response.data[i];
+      }
 
-      console.log(newData);
-      this.series[0].data = [newData];
       if (isNaN(this.series[0].data)) {
         console.log("not a number found");
         console.log(this.newData);
@@ -114,14 +118,20 @@ export default {
   created() {
     HistoryService.getUserHistoryOneMonth()
       .then((response) => {
+        console.log(response.data);
+        console.log(response.data.length);
         this.options.annotations.yaxis[0].y = response.data[0].targetLow;
         this.options.annotations.yaxis[0].y2 = response.data[0].targetHigh;
-        this.series[0].data = response.data[0].inputLevel;
-        this.newData = response.data[0];
-
-        console.log(this.newData.inputLevel);
+        for (var i = 0; i < response.data.length; i++) {
+          this.series[0].data.push(response.data[i].inputLevel);
+          this.newData = response.data;
+        }
+        console.log(response.data + "this is newData");
         this.updateChart();
+        this.series[0].data = response;
+        console.log(response);
       })
+
       .catch((error) => console.error(error));
   },
 };
