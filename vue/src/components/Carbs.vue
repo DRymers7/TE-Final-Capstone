35e862e410b2a180b029979d54a6703a156c3438
Derek Rymers <rymersd@gmail.com>
2022-12-15 11:36:11 -0500

    Committing to pull

--- vue/src/components/Carbs.vue
+++ vue/src/components/Carbs.vue
@@ -103,7 +103,7 @@ export default {
     border-radius: 10px;
     box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
 } */
-.styled-table {
+/* .styled-table {
     border-collapse: collapse;
     margin: 25px 0;
     font-size: 0.9em;
@@ -151,5 +151,5 @@ button {
     border: none;
     cursor: pointer;
     width: 100%;
-}
+} */
 </style>
