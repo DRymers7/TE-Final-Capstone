8b2141b5d3d17bcdc9541ab7f7ef9a1cc29d0b00
Mitchell Brown <19mitch99@gmail.com>
2022-12-14 20:33:39 -0500

    pulling code

--- java/src/test/java/com/techelevator/dao/JdbcUserDataDaoTest.java
+++ java/src/test/java/com/techelevator/dao/JdbcUserDataDaoTest.java
@@ -1,15 +1,20 @@
 package com.techelevator.dao;
 
 import com.techelevator.dao.jdbcdao.JdbcUserDataDao;
+import com.techelevator.model.ModelClasses.BaseInsulin;
 import com.techelevator.model.ModelClasses.UserData;
-import org.junit.Assert;
+import com.techelevator.model.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
-public class JdbcUserDataTest extends BaseDaoTests{
+public class JdbcUserDataDaoTest extends BaseDaoTests{
+
+//  byte[] byteArray = (byte);
+//  private static final UserData USER_DATA_1 = new UserData(1, 7.50, 99, 1, 47, Timestamp.valueOf("2022/12/12"), 120, 500, "Active", "614-633-9073", "614-419-3583", "Froggie", );
 
     private JdbcUserDataDao dao;
 
@@ -22,7 +27,9 @@ public class JdbcUserDataTest extends BaseDaoTests{
     @Test
     public void get_user_data() throws SQLException {
         UserData userData = dao.getUserData(1);
-        
+
+
+
 
     }
 }
